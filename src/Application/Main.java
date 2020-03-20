5a239db05dadf85b807ec0247552586f9db5b8b4
saha80 <saha80@users.noreply.github.com>
2020-03-20 12:16:15 +0300

    renaming package

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,12 +1,8 @@
-package sample;
+package Application;
 
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import MainMenu.MainMenu;
