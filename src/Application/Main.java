4b0bcc5d8667b9d0efc752fccb444444cf7e2528
saha80 <saha80@users.noreply.github.com>
2020-03-25 11:01:21 +0300

    add EventHandler

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -2,7 +2,14 @@ package Application;
 
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.event.EventHandler;
+import javafx.scene.text.*;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import MainMenu.MainMenu;
@@ -16,7 +23,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Snake");
-        //StackPane root = new StackPane();
         MainMenu mainMenu = new MainMenu();
         Scene scene = new Scene(mainMenu.getvBox(), 300, 275);
         primaryStage.setScene(scene);
--- src/MainMenu/MainMenu.java
+++ src/MainMenu/MainMenu.java
@@ -4,30 +4,46 @@ import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Insets;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MainMenu {
     private Button playButton = new Button("Play");
     private Button scoreboardButton = new Button("Score board");
     private Button optionsButton = new Button("Options");
     private Button exitButton = new Button("Exit");
-    private VBox vBox = new VBox();
+    private ImageView imageView = new ImageView(new Image(new File("\"C:\\Users\\Alex\\Desktop\\snake icon.jpg\"").toURI().toString()));
+    private VBox verticalLayout = new VBox();
 
     public MainMenu() {
+        initButtons();
+        initVBox();
+    }
+
+    public VBox getvBox() {
+        return verticalLayout;
+    }
+
+    private void initButtons() {
         playButton.setMaxWidth(Double.MAX_VALUE);
         scoreboardButton.setMaxWidth(Double.MAX_VALUE);
         optionsButton.setMaxWidth(Double.MAX_VALUE);
         exitButton.setMaxWidth(Double.MAX_VALUE);
         exitButton.setOnAction(e -> Platform.exit());
-        vBox.setSpacing(10);
-        vBox.setAlignment(Pos.CENTER);
-        final double indent = 50;
-        vBox.setPadding(new Insets(0, indent, 0, indent));
-        vBox.getChildren().addAll(playButton, scoreboardButton, optionsButton, exitButton);
     }
 
-    public VBox getvBox() {
-        return vBox;
+    private void initVBox() {
+        final double indent = 50;
+        verticalLayout.setSpacing(10);
+        verticalLayout.setAlignment(Pos.CENTER);
+        verticalLayout.setMinSize(100, 100);
+        verticalLayout.setPadding(new Insets(0, indent, 0, indent));
+        verticalLayout.getChildren().addAll(imageView, playButton, scoreboardButton, optionsButton, exitButton);
     }
 }
